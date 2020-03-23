56059fef7ecf60b859d9c091bc5104e1eb09b2b8
GerardProject <gerardklee@hotmail.com>
2020-03-22 18:04:55 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as THREE from 'three';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("cube created");
+        this.distnace = 50;
+        this.length = 1;
+        this.width = 1;
+        this.height = 1;
+        this.cameraPosition = 5;
+    }
+
+    componentDidMount() {
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(this.distance, window.innerWidth / window.innerHeight);
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+        var geometry = new THREE.BoxGeometry(this.length, this.width, this.height);
+        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        var cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        camera.position.z = this.cameraPosition;
+        var animate = function() {
+            requestAnimationFrame(animate);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera);
+        };
+        animate();
+    }
+
+    render() {
+        return (
+            <div>
+                cube created
+            </div>
+        )
+    }
 }
 
 export default App;
