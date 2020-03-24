a9a2cf4440cb385e6911d887a9db5e254f461c38
GerardProject <gerardklee@hotmail.com>
2020-03-23 21:59:03 -0700

    small fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
 import * as THREE from 'three';
+import { IconContext } from 'react-icons';
+import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowRight } from 'react-icons/fa';
+import { FaArrowDown } from 'react-icons/fa';
+import { FaArrowUp } from 'react-icons/fa';
 
 class App extends React.Component {
     constructor(props) {
@@ -18,12 +23,14 @@ class App extends React.Component {
         var camera = new THREE.PerspectiveCamera(this.distance, window.innerWidth / window.innerHeight);
         var renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+        renderer.setClearColor(0xffffff);
+        document.getElementById("cube").appendChild(renderer.domElement);
         var geometry = new THREE.BoxGeometry(this.length, this.width, this.height);
-        var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
         var cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
         camera.position.z = this.cameraPosition;
+
         var animate = function() {
             requestAnimationFrame(animate);
             cube.rotation.x += 0.01;
@@ -34,11 +41,25 @@ class App extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                cube created
+        return [
+            <div id="cube">
+                <h3>Cube goes here</h3>
+                <IconContext.Provider value={{ size: 50 }}>
+                    <span id="arrow-down">
+                        <FaArrowDown />
+                    </span>
+                    <span id="arrow-up">
+                        <FaArrowUp />
+                    </span>
+                    <span id="arrow-left">
+                        <FaArrowLeft />
+                    </span>
+                    <span id="arrow-right">
+                        <FaArrowRight />
+                    </span>
+                </IconContext.Provider>
             </div>
-        )
+        ]
     }
 }
 
